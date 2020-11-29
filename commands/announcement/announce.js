592c5bc0597dd392521bfd41f391fabce2a43937
Spicyraz <gamingmadcop@gmail.com>
2020-11-29 18:59:10 +0000

    Added announce cmd and improved whois cmd

---
+++ commands/announcement/announce.js
@@ -0,0 +1,23 @@
+const mongo = require('../../mongo')
+const announceSchema = require('../../schemas/announcement-schema')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+    commands: ['announce'],
+    expectedArgs: '<announcment>',
+    permissionError: '',
+    minArgs: 1,
+    maxArgs: null,
+    callback: async (message, arguments, text, client)  => {
+        const embed = new MessageEmbed().setColor('#900CC7')
+        .setAuthor(`Requested by ${message.author.username}!`, message.author.displayAvatarURL())
+        .setDescription(text)
+        .setFooter('?announce')
+        .setTimestamp()
+
+        message.channel.send('@everyone')
+        message.channel.send(embed)
+    },
+    permissions: 'ADMINISTRATOR',
+    requiredRoles: [],
+}
--- commands/user-info.js
+++ commands/user-info.js
@@ -1,5 +1,6 @@
 const { MessageEmbed } = require('discord.js')
 
+
 module.exports = {
     commands: ['userinfo', 'wi', 'whois'],
     expectedArgs: 'User info command',
@@ -8,41 +9,47 @@ module.exports = {
     maxArgs: null,
     callback: (message, arguments, text) => {
         const { guild, channel } = message
+        const guildId = 770371126328819754
 
         const user = message.mentions.users.first() || message.member.user
         const member = guild.members.cache.get(user.id)
+        const userStatus = member.presence.status
+        let types = [
+          "Playing",
+          "Streaming",
+          "Listening",
+          "Watching"
+        ],
+          status = {
+          "online": `🟢 Online`,
+          "idle": `🟡 Idle`,
+          "dnd": `🔴 DND`,
+          "offline": `⚫ Offline`
+        },
+          st = {
+          "desktop": "🖥 Desktop",
+          "mobile": "📱 Mobile",
+          "web": "🌐 Web"
+        }
     
         console.log(member)
+        console.log(userStatus)
     
         const embed = new MessageEmbed()
           .setColor('#900CC7')
           .setAuthor(`User info for ${user.username}`, user.displayAvatarURL())
           .addFields(
             {
-              name: 'User tag',
-              value: user.tag,
+              name: 'User',
+              value: `:white_small_square: **Name:** ${user.tag}\n:white_small_square: **Discriminator:** ${user.discriminator}\n:white_small_square: **Id:** ${user.id}\n:white_small_square: **Is bot:** ${user.bot}\n:white_small_square: **Account Created:** ${new Date(user.createdTimestamp).toLocaleDateString()}`,
             },
             {
-              name: 'Is bot',
-              value: user.bot,
+              name: 'Member',
+              value: `:white_small_square: **Joined Server:** ${new Date(member.joinedTimestamp).toLocaleDateString()}\n:white_small_square: **Nickname:** ${member.nickname || 'None'}\n:white_small_square: **Roles:** ${member.roles.cache ? member.roles.cache.map(r => `${r}`).join(' | ') : ""}`,
             },
-            {
-              name: 'Nickname',
-              value: member.nickname || 'None',
-            },
-            {
-              name: 'Joined Server',
-              value: new Date(member.joinedTimestamp).toLocaleDateString(),
-            },
-            {
-              name: 'Joined Discord',
-              value: new Date(user.createdTimestamp).toLocaleDateString(),
-            },
-            {
-              name: 'Roles',
-              value: member.roles.cache.size - 1,
-            }
           )
+          .addField('Presence', `:white_small_square: **Status:** ${status[userStatus]}`, true)
+
     
         channel.send(embed)
       },

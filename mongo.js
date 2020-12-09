2d8b6a9d2304a53a38afbb20b8a5ca703ead4827
DJMadcopp33 <ryan06y@outlook.com>
2020-12-09 17:31:11 +0000

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const { mongoPath } = require('./config.json')
 
 module.exports = async () => {
-  await mongoose.connect('mongodb+srv://Madcop:BuckyMaxMax112@discordjs.cplrv.azure.mongodb.net/Data', {
+  await mongoose.connect('mongo code here', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
